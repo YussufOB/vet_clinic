9bd2267f730850353ef22fc4caf8b6685d243829
Yussuf Bash <hamzyuss1@gmail.com>
2023-01-20 18:47:17 +0100

    querying data on logical operations

--- queries.sql
+++ queries.sql
@@ -9,3 +9,29 @@ SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT * FROM animals  WHERE neutered = TRUE;
 SELECT * FROM animals  WHERE name != 'Gabumon';
 SELECT * FROM animals  WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+/* Querying data */
+
+BEGIN;
+UPDATE animals SET species = 'digmon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
+COMMIT;
+
+BEGIN;
+DELETE FROM animals WHERE name != '';
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT delete_dob;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT delete_dob;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 1;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts < 1;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
