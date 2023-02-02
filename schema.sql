47474a33c2b808e351c46244081f0ce69a3fc65b
Yussuf Bash <hamzyuss1@gmail.com>
2023-02-02 08:50:02 +0100

    optimise the performance of animal_id search

--- schema.sql
+++ schema.sql
@@ -45,5 +45,7 @@ CREATE TABLE vets(
 );
 
 --Indexing would be better option for optimizing the query performance in this scenario cause provides a fast and efficient way to locate rows in visits and owners tables based on  specific  values (id and email columns). Denormalization may not be efficient since we are not to collect data from both tables.
+
+CREATE INDEX animal_index ON visits(animal_id);
 CREATE INDEX index_vet ON visits(vet_id);
 CREATE INDEX index_email ON owners(email);
